b689ec5a355ee58b732e182bec83beb1f23d080e
Diana-Patcelya <dianapatzela@gmail.com>
2024-04-29 23:48:43 +0300

    add hw_1

---
+++ hw_1/scripts/hw_1.js
@@ -0,0 +1,56 @@
+// --------------------------------   1   ------------------------- //
+let one = 'hello';
+let two = 'owu';
+let three = 'com'
+let four = 'ua';
+let five = 1;
+let six = 10;
+let seven = -999;
+let eight = 123;
+let nine = 3.14;
+let ten = 2.7;
+let eleven = 16;
+let twelve = true;
+let thirteen = false;
+
+console.log(one);
+console.log(two);
+console.log(three);
+console.log(four);
+console.log(five);
+console.log(six);
+console.log(seven);
+console.log(eight);
+console.log(nine);
+console.log(ten);
+console.log(eleven);
+console.log(twelve);
+console.log(thirteen);
+console.log(' ')
+
+// --------------------------------   2   ------------------------- //
+
+let firstName = "Diana";
+let middleName = 'Oleksandrivna';
+let lastName = 'Patsela';
+let space = ' ';
+let person = firstName + space + middleName + space + lastName;
+console.log(person)
+
+let firstName_2 = prompt('Enter your first name: ', 'Ivan')
+let middleName_2 = prompt('Enter your middle name: ', 'Ivanovich')
+let lastName_2 = prompt('Enter your last name: ', 'Ivanov')
+let person_2 = `${firstName_2} ${middleName_2} ${lastName_2}`;
+console.log(person_2)
+console.log(' ')
+
+
+// --------------------------------   3   ------------------------- //
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, a)
+console.log(typeof b, b)
+console.log(typeof c, c)
